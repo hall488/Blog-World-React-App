a25f943ecc3785ab4267db318f82ce337bfaed09
hall488 <hall488@purdue.edu>
2023-11-12 20:05:32 -0500

    Add redirect for errorpage

--- src/Error-Page.jsx
+++ src/Error-Page.jsx
@@ -1,27 +1,40 @@
-import { useRouteError } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function ErrorPage() {
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const error = useRouteError();
+    console.error(error);
 
-  const error = useRouteError();
-  console.error(error);
+    const clickLink = path => {
+        navigate(`/Blog-World-React-App/${path}`);
+    };
 
-  
-
-  const clickLink= (path) => {
-    navigate(`/${path}`);
-  }
-
-  return (
-    <div id="error-page" style={{display: "flex", flexDirection: "column", alignItems: "center", width: "100vw"}}>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred. Try heading back home!</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <button onClick={() => {clickLink("")}}>Go Home</button>
-    </div>
-  );
+    return (
+        <div
+            id="error-page"
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100vw',
+            }}
+        >
+            <h1>Oops!</h1>
+            <p>
+                Sorry, an unexpected error has occurred. Try heading back home!
+            </p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <button
+                onClick={() => {
+                    clickLink('');
+                }}
+            >
+                Go Home
+            </button>
+        </div>
+    );
 }
